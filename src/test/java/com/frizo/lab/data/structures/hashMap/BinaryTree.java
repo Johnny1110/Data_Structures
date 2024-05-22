78a9293583bbf21d792226b582a316f0a8b03e19
Johnny <Jarvan1110@gmail.com>
2024-05-22 23:56:06 +0800

    feat: init hashmap etc

---
+++ src/test/java/com/frizo/lab/data/structures/hashMap/BinaryTree.java
@@ -0,0 +1,6 @@
+package com.frizo.lab.data.structures.hashMap;
+
+/**
+* 二元樹實現
+*/
+public class BinaryTree {}
---
+++ src/test/java/com/frizo/lab/data/structures/hashMap/RBTree.java
@@ -0,0 +1,6 @@
+package com.frizo.lab.data.structures.hashMap;
+
+/**
+ * 紅黑樹實現
+ */
+public class RBTree {}
